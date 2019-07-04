69d873db64a1b61566e15357c4009f93ec247c1e
“Kchu <your_registerl>
2019-07-04 11:49:16 +0800

    Kchu

--- Distributional_RL/result_show.py
+++ Distributional_RL/result_show.py
@@ -3,12 +3,7 @@ import pickle
 import matplotlib.pyplot as plt
 
 # paths for result log
-# RESULT_PATH = ['./data/plots/quota_result.pkl','./data/plots/C51_result.pkl',\
-# 				'./data/plots/qr-dqn_result.pkl','./data/plots/iqn_result.pkl',\
-# 				'./data/plots/aqn_result.pkl']
-RESULT_PATH = ['./data/plots/quota_result.pkl',
-				'./data/plots/iqn_result.pkl','./data/plots/aqn_result.pkl',\
-				'./data/plots/aqn_result_1.pkl','./data/plots/aqn_result_2.pkl']
+RESULT_PATH = ['./data/plots/quota_result.pkl','./data/plots/iqn_result.pkl',]
 
 # model load with check
 result = []
@@ -24,17 +19,12 @@ for i in range(len(RESULT_PATH)):
 print('Load complete!')
 print('Plotting the curves!')
 
-# plt.plot(range(len(result[0])), result[0], label="QUOTA")
-# plt.plot(range(len(result[1])), result[1], label="C51")
-# plt.plot(range(len(result[2])), result[2], label="QR-DQN")
+plt.plot(range(len(result[0])), result[0], label="QUOTA")
 plt.plot(range(len(result[1])), result[1], label="IQN")
-# plt.plot(range(len(result[2])), result[2], label="AQN")
-# plt.plot(range(len(result[3])), result[3], label="AQN_1")
-plt.plot(range(len(result[4])), result[4], label="AQN_2")
 
 plt.legend()
-plt.xlabel('iteration times')
-plt.ylabel('score')
+plt.xlabel('Iteration times(Thousands)')
+plt.ylabel('Score')
 plt.tight_layout()
 plt.grid()
 plt.show()
